4f5858d25840a22d4107564597e349b6b9cbdadf
Isha Gaikwad <gaikwadisha541@gmail.com>
2026-01-05 16:52:42 +0000

    Variables practice

---
+++ 01_Basics/Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144552
+let accountEmail = "isha21@gmail.com"
+var accountPass = "4356"
+accountCity = "Mumbai"
+
+// accountId = 2   not allowed
+accountEmail = "icici@gmail.com"
+accountPass = "3322"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity])
