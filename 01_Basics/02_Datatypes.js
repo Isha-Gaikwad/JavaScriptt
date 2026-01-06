acf47d785ebd02818493a880cbff6ad8b43d44d6
Isha Gaikwad <gaikwadisha541@gmail.com>
2026-01-06 14:27:04 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as a newer version 
+
+console.log(3+3); 
+let name = "Isha"
+let age = 18;
+let isLoggedIn = false
+
+
+// primitiveDatatypes
+//number
+//bigint used for big numers
+//String
+//boolean
+//null
+//udefined
+//symbol
+
+//non-primirive datatypes
+
+//1. object
+
+
