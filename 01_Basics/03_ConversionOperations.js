6776ac0aeab228a8d62bd99cd7226a5b5b182ea6
Isha Gaikwad <gaikwadisha541@gmail.com>
2026-01-06 15:23:39 +0000

    conversion

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,22 @@
+// converting the value in number for that we use ValueInNumber so that anything can convert
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Convert boolean 
+let isLoggedIn = 1
+
+let booleanIsLoggdIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggdIn)
+
+//string conversion 
+
+let tkNumber = 44;
+let isha = String(tkNumber)
+console.log(isha)
+console.log(typeof(isha))
