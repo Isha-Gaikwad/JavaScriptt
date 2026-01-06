ae8df98b9b6bfaebe3d8e122250ddf094e8dfdb8
Isha Gaikwad <gaikwadisha541@gmail.com>
2026-01-06 18:05:21 +0000

    complex operations

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,51 @@
+// operations which is complex
+let Sum = 55
+let negValue = -Sum
+console.log(negValue);
+
+// Basics operations
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+*/
+/*
+let str1 = "hello"
+let str2 = " Isha Gaikwad"
+
+let str3 = str1+str2
+console.log(str3);
+*/
+
+// this one conversion part where it convert string into numbers ad give proper output.
+
+/*console.log("1" + 2);
+console.log(1 +"2");
+console.log("1"+2+2);
+console.log(1+2+"2");*/
+
+// tricky conversion (je ki nhi kele pahije tyane code readable nhi dist ani smjaila hard jatoo)
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
